14f1e10e877f57f44941f51e824211f0026e840c
Matthew Heon <matthew.heon@pm.me>
2020-04-27 10:36:41 -0400

    Add support for volumes-from, image volumes, init
    This should complete Podmanv2's support for volume-related flags.
    Most code was sourced from the old pkg/spec implementation with
    modifications to account for the split between frontend flags
    (volume, mount, tmpfs) and the backend flags implemented here.
    
    Also enables tests for podman run with volumes
    
    Signed-off-by: Matthew Heon <matthew.heon@pm.me>

--- test/e2e/run_volume_test.go
+++ test/e2e/run_volume_test.go
@@ -28,7 +28,6 @@ var _ = Describe("Podman run with volumes", func() {
 	)
 
 	BeforeEach(func() {
-		Skip(v2fail)
 		tempdir, err = CreateTempDirInTempDir()
 		if err != nil {
 			os.Exit(1)
@@ -224,6 +223,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("podman run with tmpfs named volume mounts and unmounts", func() {
+		Skip(v2fail)
 		SkipIfRootless()
 		volName := "testvol"
 		mkVolume := podmanTest.Podman([]string{"volume", "create", "--opt", "type=tmpfs", "--opt", "device=tmpfs", "--opt", "o=nodev", "testvol"})
@@ -280,6 +280,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("podman named volume copyup", func() {
+		Skip(v2fail)
 		baselineSession := podmanTest.Podman([]string{"run", "--rm", "-t", "-i", ALPINE, "ls", "/etc/apk/"})
 		baselineSession.WaitWithDefaultTimeout()
 		Expect(baselineSession.ExitCode()).To(Equal(0))
@@ -311,6 +312,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("podman run with anonymous volume", func() {
+		Skip(v2fail)
 		list1 := podmanTest.Podman([]string{"volume", "list", "--quiet"})
 		list1.WaitWithDefaultTimeout()
 		Expect(list1.ExitCode()).To(Equal(0))
@@ -329,6 +331,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("podman rm -v removes anonymous volume", func() {
+		Skip(v2fail)
 		list1 := podmanTest.Podman([]string{"volume", "list", "--quiet"})
 		list1.WaitWithDefaultTimeout()
 		Expect(list1.ExitCode()).To(Equal(0))
@@ -357,6 +360,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("podman rm -v retains named volume", func() {
+		Skip(v2fail)
 		list1 := podmanTest.Podman([]string{"volume", "list", "--quiet"})
 		list1.WaitWithDefaultTimeout()
 		Expect(list1.ExitCode()).To(Equal(0))
@@ -395,6 +399,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("podman mount with invalid option fails", func() {
+		Skip(v2fail)
 		volName := "testVol"
 		volCreate := podmanTest.Podman([]string{"volume", "create", "--opt", "type=tmpfs", "--opt", "device=tmpfs", "--opt", "o=invalid", volName})
 		volCreate.WaitWithDefaultTimeout()
@@ -465,6 +470,7 @@ var _ = Describe("Podman run with volumes", func() {
 	})
 
 	It("Podman fix for CVE-2020-1726", func() {
+		Skip(v2fail)
 		volName := "testVol"
 		volCreate := podmanTest.Podman([]string{"volume", "create", volName})
 		volCreate.WaitWithDefaultTimeout()
